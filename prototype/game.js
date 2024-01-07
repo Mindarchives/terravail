97c0e63156e4490abc51cf37a4563e77d738bd91
mindarchives <davylong25@gmail.com>
2024-01-07 08:14:13 +0100

    start level one map

--- prototype/game.js
+++ prototype/game.js
@@ -1,5 +1,36 @@
 window.onload = onLoad
 
 function onLoad(){
-    console.log("hello world")
+    startNewGame();
+}
+
+const maps = {
+    1: [
+        [1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1],
+    ]
+}
+
+function displayMap(level){
+    console.log("now displaying map for level" + " " + level);
+  
+  for(let i = 0; i < maps[level].length; i++){
+    displayMapRow(maps[level][i])
+  }  
+}
+
+function startNewGame(){
+    displayMap(1);
+}
+function displayMapRow(mapRow){
+    const gameRoot = document.getElementById("game-root");
+    
 }
